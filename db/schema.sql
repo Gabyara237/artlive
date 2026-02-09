2f5bff1ce77f636f023aeba952686a93030c8217
Gabriela Araujo <gabyara237@gmail.com>
2026-02-09 14:04:40 -0800

    feat: add initial database schema with users and roles

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE artlive;
+\c artlive
+
+DROP TABLE IF EXISTS users CASCADE;
+
+DROP TYPE IF EXISTS user_role CASCADE;
+
+CREATE TYPE user_role AS ENUM ('instructor', 'student');
+
+
+CREATE TABLE users(
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    full_name VARCHAR(100) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    bio TEXT,
+    profile_image TEXT,
+    role user_role NOT NULL,
+    specialties TEXT[] DEFAULT '{}',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
